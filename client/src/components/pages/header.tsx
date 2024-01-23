136c3d87960b7432e631836978bd0bc4796259a7
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2024-01-23 16:53:40 +0200

    using logo ad adding color to theme

--- client/src/components/pages/header.tsx
+++ client/src/components/pages/header.tsx
@@ -1,9 +1,11 @@
-import { Flex, Text } from '../ui';
+import { Logo } from '../../styles/icons';
+import { Flex, Svg, Text } from '../ui';
 
 export function Header() {
   return (
-    <Flex alignItems="center" fontSize={3} height="60px" bg="main.cyanBlue">
-      <Text pl="25px">Header</Text>
+    <Flex color="main.white" alignItems="center" fontSize={3} height="60px" bg="main.cyanBlue" pl="25px">
+      <Svg as={Logo} height="35px" width="35px" />
+      <Text pl="13px">Get Together</Text>
     </Flex>
   );
 }
--- client/src/styles/theme.tsx
+++ client/src/styles/theme.tsx
@@ -4,6 +4,7 @@ export const theme: DefaultTheme = {
   colors: {
     main: {
       cyanBlue: '#015cc7',
+      white: '#FFF',
     },
   },
   fontSizes: ['10px', '12px', '14px', '16px', '18px', '20px'],
