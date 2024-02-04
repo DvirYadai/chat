f8fdff80e83534f5cdde45d6d59a2bda3f040271
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2024-02-04 20:33:41 +0200

    verifying user when enter main page

--- client/src/components/pages/main.tsx
+++ client/src/components/pages/main.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
 import { Sidebar } from '../sidebar';
 import { Box, Flex } from '../ui';
 import { Header } from '../header';
 import { ChatView } from '../chat-view';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux-hooks';
+import { verify } from '../../slices/authSlice';
 
 export function Main() {
+  const basicUserInfo = useAppSelector((state) => state.auth.basicUserInfo);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const verifyFunc = async () => {
+      await dispatch(verify(basicUserInfo)).unwrap();
+    };
+    verifyFunc();
+  });
+
   return (
     <Box height="100%" width="100%" fontFamily="Kanit">
       <Header />
