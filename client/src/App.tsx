8eba7dc490d0e51034ecc7c66ef9999670728572
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2024-02-04 20:31:47 +0200

    creating protected layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,41 @@
 import { ThemeProvider } from 'styled-components';
-import { Route, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom';
+import { Navigate, Outlet, Route, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { theme } from './styles';
 import { SignUp, Login, Main } from './components/pages';
+import store from './store';
+import { useAppSelector } from './hooks/redux-hooks';
+
+function ProtectedLayout() {
+  const basicUserInfo = useAppSelector((state) => state.auth.basicUserInfo);
+
+  if (!basicUserInfo) {
+    return <Navigate replace to="/login" />;
+  }
+
+  return <Outlet />;
+}
+
+function DefaultLayout() {
+  const basicUserInfo = useAppSelector((state) => state.auth.basicUserInfo);
+
+  if (basicUserInfo) {
+    return <Navigate replace to="/" />;
+  }
+
+  return <Outlet />;
+}
 
 function Root() {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/sign_up" element={<SignUp />} />
+      <Route element={<ProtectedLayout />}>
+        <Route path="/" element={<Main />} />
+      </Route>
+      <Route element={<DefaultLayout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign_up" element={<SignUp />} />
+      </Route>
     </Routes>
   );
 }
@@ -17,9 +44,11 @@ const router = createBrowserRouter([{ path: '*', Component: Root }]);
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
