e53e8037ca0f838ca5fde7b9964e35cc25d45f16
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2024-02-04 20:33:24 +0200

    fixing login and register and adding verify user route

--- server/controllers/userController.tsx
+++ server/controllers/userController.tsx
@@ -19,27 +19,26 @@ const userRepository = AppDataSource.getRepository(User)
 const signup_post = async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
   try {
-    const existUser = await userRepository.findOneBy({
-        username,
-    })
-    if (existUser) {
-      return res.status(409).send("Username already exist");
-    }
     const existEmail = await userRepository.findOneBy({
         email,
     })
     if (existEmail) {
       return res.status(409).send("Email already exist");
     }
+    const existUser = await userRepository.findOneBy({
+        username,
+    })
+    if (existUser) {
+      return res.status(409).send("Username already exist");
+    }
     const newUser = new User()
     newUser.email = email;
     newUser.username = username;
     newUser.password_hash = hashSync(password, genSaltSync(10))
-    await userRepository.save(newUser)
-    newUser.password_hash = "password";
-    const token = createToken(newUser);
+    const userCreated = await userRepository.save(newUser)
+    const token = createToken(userCreated);
     res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
-    res.status(201).json({ user: username });
+    res.status(201).json({ id: userCreated.id, username: userCreated.username, email: userCreated.email });
   } catch (error) {
     return res.status(500).send(error);
   }
@@ -59,13 +58,9 @@ const login_post = async (req: Request, res: Response) => {
     if (!auth) {
       return res.status(404).send("Incorrect email or password");
     }
-    const userToken = {
-      username: existUser.username,
-      email: existUser.email,
-    };
-    const token = createToken(userToken);
+    const token = createToken(existUser);
     res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
-    res.status(200).json({ user: userToken.username });
+    res.status(200).json({ id: existUser.id, username: existUser.username, email: existUser.email });
   } catch (error) {
     return res.status(500).send(error);
   }
@@ -76,19 +71,28 @@ const logout_get = (req: Request, res: Response) => {
   res.status(200).send("user logged out");
 };
 
-const token_get = (req: Request, res: Response) => {
+const verify_user = async (req: Request, res: Response) => {
+  const { id = '', email = '', username = '' } = req.body;
+  const existUser = await userRepository.findBy({
+    id,
+    email,
+    username,
+  });
+  if (!existUser.length) {
+    return res.status(401).send("Unauthorized user");
+  }
   if (req.cookies.jwt) {
     const token = req.cookies.jwt;
     jwt.verify(token, process.env.SECRET_KEY as string, async (err: VerifyErrors | null, decodedToken?: any) => {
       if (err) {
         return res.status(403).send("Invalid access Token");
       } else {
-        return res.status(200).json({ user: decodedToken?.user.username });
+        return res.status(200).json({});
       }
     });
   } else {
     return res.status(401).send("Access Token Required");
   }
-};
+}
 
-export { signup_post, login_post, logout_get, token_get }
+export { signup_post, login_post, logout_get, verify_user }
--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { login_post, logout_get, signup_post, token_get } from "../controllers/userController";
+import { login_post, logout_get, signup_post, verify_user } from "../controllers/userController";
 export const router = Router();
 
 router.post("/signup", signup_post);
 router.post("/login", login_post);
+router.post("/verify", verify_user);
 router.get("/logout", logout_get);
-router.get("/token", token_get);
