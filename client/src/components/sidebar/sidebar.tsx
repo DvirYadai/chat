20fd5fd96380c17ef08fa1d2c0a64bbce64444b5
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2024-02-10 23:35:03 +0200

    adding userSlice and state for sidebar

--- client/src/components/sidebar/sidebar.tsx
+++ client/src/components/sidebar/sidebar.tsx
@@ -1,18 +1,38 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { GroupIcon, MessageIcon, PhoneIcon, SettingsIcon } from '../../styles/icons';
 import { Box, Flex, Svg } from '../ui';
 import { Chats } from './chats';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux-hooks';
+import { switchSidebarView } from '../../slices/userSlice';
 
-export const StyledSvg = styled(Svg)`
-  cursor: pointer;
-  &:hover {
+export const StyledSvg = styled(Svg)<{ isActive?: boolean }>`
+  ${({ isActive }) => css`
+    cursor: pointer;
+    &:hover {
+      path {
+        fill: url(#myGradient);
+      }
+    }
+    ${isActive &&
+    `
     path {
       fill: url(#myGradient);
     }
-  }
+    `}
+  `}
 `;
 
+const sidebarMenuIconsMap = {
+  chats: MessageIcon,
+  calls: PhoneIcon,
+  groups: GroupIcon,
+  settings: SettingsIcon,
+};
+
 function Menu() {
+  const { activeSidebarMenuItem } = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+
   return (
     <Flex
       justifyContent="space-around"
@@ -22,19 +42,28 @@ function Menu() {
       bg="main.white"
       p="0 10px"
     >
-      <StyledSvg as={MessageIcon} />
-      <StyledSvg as={PhoneIcon} />
-      <StyledSvg as={GroupIcon} />
-      <StyledSvg as={SettingsIcon} />
+      {Object.entries(sidebarMenuIconsMap).map(([key, icon]) => {
+        const isActive = activeSidebarMenuItem === key;
+        return <StyledSvg onClick={() => dispatch(switchSidebarView(key))} isActive={isActive} key={key} as={icon} />;
+      })}
     </Flex>
   );
 }
 
+const sidebarMenuItemsMap = {
+  chats: <Chats />,
+  calls: <>calls</>,
+  groups: <>groups</>,
+  settings: <>settings</>,
+};
+
 export function Sidebar() {
+  const { activeSidebarMenuItem } = useAppSelector((state) => state.user);
+
   return (
     <Box width="325px" bg="#f8fafc">
       <Menu />
-      <Chats />
+      {sidebarMenuItemsMap[activeSidebarMenuItem]}
     </Box>
   );
 }
--- client/src/slices/userSlice.ts
+++ client/src/slices/userSlice.ts
@@ -1,9 +1,73 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { UserBasicInfo } from './authSlice';
+import axiosInstance from '../axiosInstance';
+
+type Message = {
+  id: number;
+  content: string;
+  timestamp: string;
+  senderID: number;
+};
+
+type Chat = {
+  id: number;
+  messages: Array<Message>;
+  user1Info: UserBasicInfo;
+  user2Info: UserBasicInfo;
+  lastMessage: Message | undefined;
+};
+
+type UserState = {
+  activeSidebarMenuItem: 'chats' | 'groups' | 'calls' | 'settings';
+  selectedChat: number | null;
+  chats: Array<Chat>;
+  status: 'idle' | 'loading' | 'failed';
+  error: string | null;
+};
+
+const initialState: UserState = {
+  activeSidebarMenuItem: 'chats',
+  selectedChat: null,
+  chats: [],
+  status: 'idle',
+  error: null,
+};
+
+export const getAllData = createAsyncThunk('getAllData', async () => {
+  try {
+    const response = await axiosInstance.get('/data/all');
+    const resData = response.data;
+    return resData;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+});
 
 const userSlice = createSlice({
   name: 'user',
-  initialState: '',
-  reducers: {},
+  initialState,
+  reducers: {
+    switchSidebarView: (state, action) => {
+      state.activeSidebarMenuItem = action.payload;
+    },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(getAllData.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(getAllData.fulfilled, (state, action: PayloadAction<Array<Chat>>) => {
+        state.status = 'idle';
+        state.chats = action.payload;
+      })
+      .addCase(getAllData.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message || 'Fetch data failed';
+      });
+  },
 });
 
+export const { switchSidebarView } = userSlice.actions;
 export default userSlice.reducer;
